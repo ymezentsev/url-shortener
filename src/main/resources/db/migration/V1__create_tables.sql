3d8d93343cada6ade04dccc9afdc80a924f88795
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-01-23 17:06:16 +0200

    fix templates

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -2,13 +2,14 @@ CREATE TABLE IF NOT EXISTS users (
     id       BIGSERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE CHECK (LENGTH(username) >= 2),
     password VARCHAR(100) NOT NULL,
-    enabled  BOOLEAN      NOT NULL,
     role     VARCHAR(20)  NOT NULL DEFAULT 'USER'
 );
 
 CREATE TABLE IF NOT EXISTS urls (
-    short_url       VARCHAR(8) PRIMARY KEY CHECK (length(short_url) = 8),
+    id              BIGSERIAL PRIMARY KEY,
+    short_url       VARCHAR(20) NOT NULL,
     url             VARCHAR(255) NOT NULL,
+    description     VARCHAR(1000),
     user_id         BIGSERIAL    NOT NULL,
     created_date    TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
     expiration_date TIMESTAMP  NOT NULL,
