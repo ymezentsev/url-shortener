1e0c71763424b5da22b524f57f452bccb64a4af1
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-01-25 15:31:39 +0200

    fix ShortLinkGenerator

--- src/main/java/ua/goit/urlshortener/url/UrlRepository.java
+++ src/main/java/ua/goit/urlshortener/url/UrlRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import ua.goit.urlshortener.url.UrlEntity;
 
 import java.time.LocalDate;
 import java.util.List;
--- src/main/java/ua/goit/urlshortener/url/service/ShortLinkGenerator.java
+++ src/main/java/ua/goit/urlshortener/url/service/ShortLinkGenerator.java
@@ -14,18 +14,12 @@ public class ShortLinkGenerator {
 
     public String generateShortLink() {
         Random random = new SecureRandom();
-        String shortLink;
-
 
         StringBuilder linkBuilder = new StringBuilder();
         for (int i = 0; i < LINK_LENGTH; i++) {
             int randomIndex = random.nextInt(CHARACTERS.length());
-            char randomChar = CHARACTERS.charAt(randomIndex);
-            linkBuilder.append(randomChar);
+            linkBuilder.append(CHARACTERS.charAt(randomIndex));
         }
-
-        shortLink = linkBuilder.toString();
-
-        return shortLink;
+        return linkBuilder.toString();
     }
 }
