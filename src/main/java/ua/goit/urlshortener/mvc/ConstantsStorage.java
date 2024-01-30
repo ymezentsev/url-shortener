a087b5de87bf85b8114aa21d8130bef2a4617cfc
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-01-30 15:41:55 +0200

    change ConstantStorage

--- src/main/java/ua/goit/urlshortener/mvc/ConstantsStorage.java
+++ src/main/java/ua/goit/urlshortener/mvc/ConstantsStorage.java
@@ -1,22 +1,26 @@
 package ua.goit.urlshortener.mvc;
 
 public final class ConstantsStorage {
-    public final static String MODEL_INDEX = "index";
-    public final static String MODEL_EDIT = "edit";
-    public final static String MODEL_CREATE = "create";
-    public final static String MODEL_REGISTER = "register";
-    public final static String MODEL_LOGIN = "login";
-    public final static String MODEL_SUCCESS = "success";
-    public final static String MODEL_ALL_GUEST = "all-guest";
-    public final static String MODEL_ALL_USER = "all-user";
-    public final static String MODEL_ADMIN_URLS = "admin-urls";
-    public final static String MODEL_ADMIN_USERS = "admin-users";
-    public final static String ATTRIBUTE_USERNAME = "username";
-    public final static String ATTRIBUTE_USERS = "users";
-    public final static String ATTRIBUTE_USER_URLS = "userUrls";
-    public final static String ATTRIBUTE_USER_URLS_INACTIVE = "userUrlsInactive";
-    public final static String ATTRIBUTE_URLS = "urls";
-    public final static String ATTRIBUTE_ID = "id";
-    public final static String ATTRIBUTE_FROM_ADMIN_PAGE = "fromAdminPage";
-    public final static String ATTRIBUTE_ERRORS = "errors";
+    private ConstantsStorage() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String MODEL_INDEX = "index";
+    public static final String MODEL_EDIT = "edit";
+    public static final String MODEL_CREATE = "create";
+    public static final String MODEL_REGISTER = "register";
+    public static final String MODEL_LOGIN = "login";
+    public static final String MODEL_SUCCESS = "success";
+    public static final String MODEL_ALL_GUEST = "all-guest";
+    public static final String MODEL_ALL_USER = "all-user";
+    public static final String MODEL_ADMIN_URLS = "admin-urls";
+    public static final String MODEL_ADMIN_USERS = "admin-users";
+    public static final String ATTRIBUTE_USERNAME = "username";
+    public static final String ATTRIBUTE_USERS = "users";
+    public static final String ATTRIBUTE_USER_URLS = "userUrls";
+    public static final String ATTRIBUTE_USER_URLS_INACTIVE = "userUrlsInactive";
+    public static final String ATTRIBUTE_URLS = "urls";
+    public static final String ATTRIBUTE_ID = "id";
+    public static final String ATTRIBUTE_FROM_ADMIN_PAGE = "fromAdminPage";
+    public static final String ATTRIBUTE_ERRORS = "errors";
 }
