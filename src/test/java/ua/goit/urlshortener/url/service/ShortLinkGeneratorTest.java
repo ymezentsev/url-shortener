c74e109967d21407abafac85e5113f55cd0dafec
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-02-02 21:14:46 +0200

    add test for ShortLinkGenerator

--- src/test/java/ua/goit/urlshortener/url/service/ShortLinkGeneratorTest.java
+++ src/test/java/ua/goit/urlshortener/url/service/ShortLinkGeneratorTest.java
@@ -1,61 +1,14 @@
 package ua.goit.urlshortener.url.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import ua.goit.urlshortener.url.UrlDto;
 
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class ShortLinkGeneratorTest {
-
-    private List<UrlDto> mockUrls;
-
-    @BeforeEach
-    void setUp() {
-        mockUrls = Arrays.asList(
-                createUrlDto(1L, "abc123", "https://www.example.com/1", "null", "navi",
-                        LocalDate.now(), LocalDate.now().plusDays(30L), 0),
-                createUrlDto(2L, "def456", "https://www.example.com/2", "null", "Arih",
-                        LocalDate.now(), LocalDate.now().plusDays(30L), 0),
-                createUrlDto(3L, "uthg0909", "https://www.example.com/2", "null", "Daf",
-                        LocalDate.now(), LocalDate.now().plusDays(30L), 0)
-        );
-    }
-
-    private UrlDto createUrlDto(long id, String shortUrl, String url, String description, String username,
-                                LocalDate createdDate, LocalDate expirationDate, int visitCount) {
-        UrlDto urlDto = new UrlDto();
-        urlDto.setId(id);
-        urlDto.setShortUrl(shortUrl);
-        urlDto.setUrl(url);
-        urlDto.setDescription(description);
-        urlDto.setUsername(username);
-        urlDto.setCreatedDate(createdDate);
-        urlDto.setExpirationDate(expirationDate);
-        urlDto.setVisitCount(visitCount);
-        return urlDto;
-    }
+class ShortLinkGeneratorTest {
 
     @Test
-    public void testGenerateShortLink() {
-        UrlService urlService = Mockito.mock(UrlService.class);
-
-        Mockito.when(urlService.getAll()).thenReturn(mockUrls);
-
+    void generateShortLinkTest() {
         ShortLinkGenerator shortLinkGenerator = new ShortLinkGenerator();
-
-        String generatedLink = shortLinkGenerator.generateShortLink();
-
-
-        assertTrue(generatedLink.startsWith(""));
-
-
-        assertFalse(urlService.getAll().stream().anyMatch(urlEntity -> urlEntity.getShortUrl().equals(generatedLink)));
+        assertEquals(8, shortLinkGenerator.generateShortLink().length());
     }
 }
