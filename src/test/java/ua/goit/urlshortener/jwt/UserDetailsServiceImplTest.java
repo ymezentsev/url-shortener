5cd06f81df97864166fdb6dcc8e97b2a48eab293
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-02-08 17:24:24 +0200

    add tests for UserDetailsService

---
+++ src/test/java/ua/goit/urlshortener/jwt/UserDetailsServiceImplTest.java
@@ -0,0 +1,41 @@
+package ua.goit.urlshortener.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+class UserDetailsServiceImplTest {
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+
+    @Test
+    void loadUserByUsernameTest() {
+        UserDetails userDetails = userDetailsService.loadUserByUsername("testadmin");
+        assertAll(
+                () -> assertEquals("ADMIN",
+                        userDetails.getAuthorities().stream().findFirst().orElseThrow().getAuthority()),
+                () -> assertEquals("{noop}qwerTy12", userDetails.getPassword())
+        );
+    }
+
+    @Test
+    void loadUserByUsernameThrowUsernameNotFoundExceptionTest() {
+        assertThrows(UsernameNotFoundException.class,
+                () -> userDetailsService.loadUserByUsername("test"));
+    }
+}
